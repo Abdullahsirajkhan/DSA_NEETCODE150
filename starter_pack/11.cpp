c667102461bd73da89372c5bc0d134ac1601da9e
Abdullah Siraj Khan <abdullahsirajkhan2@gmail.com>
2026-06-15 23:15:00 +0500

    11.cpp push

---
+++ starter_pack/11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+
+    using std::cin;
+    using std::cout;
+    using std::string;
+    using std::endl;
+
+    int input=0,largest=INT32_MIN,second=INT32_MIN;
+    cout<<"please start giving values input \n";
+
+    while(input!=-1){
+        cin>>input;
+        if(input==-1){
+            break;
+        }
+        if(input>largest){
+            second=largest;
+            largest=input;
+        }
+        if(input>second && input<largest){
+            second=input;
+        }   
+    }
+
+    if(second==INT32_MIN){
+        cout<<"Not enough numbers given!"<<endl;
+    }
+    else{
+        cout<<" "<<second;
+    }
+
+    return 0;
+}
