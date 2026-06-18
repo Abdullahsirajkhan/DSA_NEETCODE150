88b9195656003c5d6a6aa3d60693c22ea6cb4e89
Abdullah Siraj Khan <abdullahsirajkhan2@gmail.com>
2026-06-18 22:28:15 +0500

    push 20th

---
+++ starter_pack/20.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+    using std::cin;
+    using std::cout;
+    using std::string;
+    using std::endl;
+
+    int n=0;
+    cout<<"Enter n : \n";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j<i){
+                cout<<"  ";
+            }
+            else{
+                cout<<i<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
