9f9a6668b9ee605c4e78e709b2b6359fb4444da6
Abdullah Siraj Khan <abdullahsirajkhan2@gmail.com>
2026-06-18 16:47:38 +0500

    added 13.cpp

---
+++ starter_pack/13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+
+    using std::cin;
+    using std::cout;
+    using std::string;
+    using std::endl;
+
+    int n=0;
+    char c = 'A';
+    cout<<" Enter n : \n";
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n;j++){
+            cout<<static_cast<char>(j+c)<<" ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
