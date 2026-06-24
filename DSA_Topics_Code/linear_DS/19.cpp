f30eedd61c1a7d2482130fb0fb98344ceaffe598
Abdullah Siraj Khan <abdullahsirajkhan2@gmail.com>
2026-06-24 18:01:08 +0500

    LeetCode hard try

---
+++ DSA_Topics_Code/linear_DS/19.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_subarrsys(int* arr1, int* arr2, int n , int k){
+    int Max=INT32_MIN, sum=0;
+    int dup=; 
+    
+    for(int i=0;i<n;i++){
+        if(i<k){
+            sum+=arr1[i];
+            arr2[arr1[i]]+=1;
+            if(arr2[arr1[i]]>1){
+                check=0;
+            }
+            if(i==k-1&&check){
+                Max=sum;                
+            }
+        }
+
+        else{
+            sum+=arr1[i] -arr1[i-k];
+            arr2[arr1[i-k]]-=1;
+            arr2[arr1[i]]+=1;
+            if(arr2[arr1[i]]==1&&check){
+                Max=max(Max,sum);
+            }
+            else if(arr2[arr1[i]]==1&&i>=2*k){
+                Max=max(Max,sum);
+            }
+        }        
+    }
+    return Max;
+}
+
+
+int main(){
+
+    int n=0;;
+    cout<<"Enter the n : \n";
+    cin>>n;
+
+    int* arr1 = new int[n]();
+    int nums[] = {1, 5, 4, 2, 9, 9, 9};
+
+    int size = 7;
+    int k = 3;
+
+    cout << "Maximum sum of distinct subarray: " << max_subarrsys(nums,arr1, size, k) << endl; // Expected: 15
+
+
+
+    
+    
+    
+    return 0;
+}
